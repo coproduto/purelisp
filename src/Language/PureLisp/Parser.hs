1dbd97c2016149d8d8da3ef2f5172e68d85c1a5f
Pedro Castilho <cstl.px@gmail.com>
2021-05-29 19:09:01 -0300

    Added position metadata to parser

--- src/Language/PureLisp/Parser.hs
+++ src/Language/PureLisp/Parser.hs
@@ -1,25 +1,46 @@
 module Language.PureLisp.Parser where
 
+import Control.Monad
+
 import Text.Parsec
+import Text.Parsec.Prim
+
+data InputMetadata = InputMetadata
+  { metadataLine :: Int
+  , metadataColumn :: Int
+  } deriving Show
 
 data LispInput
-  = Atom String
+  = Atom InputMetadata String
   | Pair LispInput LispInput
   deriving Show
 
 type Parser = Parsec String ()
 
+getInputMetadata :: Parser InputMetadata
+getInputMetadata = do
+  position <- statePos `liftM` getParserState
+  return $ InputMetadata
+    { metadataLine = sourceLine position
+    , metadataColumn = sourceColumn position
+    }
+
 parseAtom :: Parser LispInput
 parseAtom = do
+  meta <- getInputMetadata
   first <- letter
   rest <- many (letter <|> digit)
-  return $ Atom (first:rest)
+  return $ Atom meta (first:rest)
 
 parseHsList :: Parser [LispInput]
 parseHsList = parseLispInput `sepBy` spaces
 
 parseConsListItems :: Parser LispInput
-parseConsListItems = foldr Pair (Atom "nil") <$> parseHsList
+parseConsListItems = do
+  pos <- getPosition
+  list <- parseHsList
+  meta <- getInputMetadata
+  return $ foldr Pair (Atom meta "nil") list
 
 parseConsList :: Parser LispInput
 parseConsList = do
